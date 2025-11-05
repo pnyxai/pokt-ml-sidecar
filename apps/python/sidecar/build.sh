442b33f6af5e76947b04d307e4085ab7b9850c42
Ramiro Rodriguez Colmeiro <rawthil@gmail.com>
2025-11-05 12:41:47 -0300

    Code cleanup

--- apps/python/sidecar/build.sh
+++
@@ -1,9 +0,0 @@
-DEFAULT_IMAGE_NAME="pokt_ml_sidecar"
-
-# go to root directory
-cd ../../..
-
-# Build sidecar
-docker build . -f apps/python/sidecar/Dockerfile --progress=plain --tag $DEFAULT_IMAGE_NAME:dev
-# Broadcast image name and tag
-echo "$DEFAULT_IMAGE_NAME"
